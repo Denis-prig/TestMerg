47c42201cde7d4824ef6b1e50191a6f3dc6a6788
Denis <67637009+Denis-prig@users.noreply.github.com>
2022-09-19 22:52:46 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import javax.swing.event.ListDataEvent;
 
 public class Main {
     public static void main(String[] args) {
-
+System.out.println(12);
     }
                            
                 
