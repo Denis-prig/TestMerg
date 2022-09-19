4c4712cefb682480d531a0e073fe35a838a82068
denis <arishind@gmail.com>
2022-09-19 22:54:40 +0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,14 @@ import javax.swing.event.ListDataEvent;
 
 public class Main {
     public static void main(String[] args) {
-System.out.println(12);
+        System.out.println(11000);
+
+
+
+
+
+        System.out.println(11);
+System.out.println(11);
     }
                            
                 
