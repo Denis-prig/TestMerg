346d0ec6ec9bf3fedfefc5752ff545c11c1b29ae
denis <arishind@gmail.com>
2022-09-19 22:35:14 +0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,12 @@ import javax.swing.event.ListDataEvent;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(10;
-        System.out.println(10);
-        System.out.println(110;
-        int x = 10;
         System.out.println(10);
         System.out.println(10);
-        System.out.println(110;
-        System.out.println(10);
-        System.out.println(10);
-        System.out.println(110;
+        System.out.println(110);
 
 
 
-        double x1 = 10;
+        float x1 = 12;
     }
 }
