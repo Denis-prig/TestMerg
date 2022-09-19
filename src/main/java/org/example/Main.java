b43de2f18a4b0116896b69312758c93bf10e64a8
Denis <67637009+Denis-prig@users.noreply.github.com>
2022-09-19 23:05:22 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,24 +5,7 @@ import javax.swing.event.ListDataEvent;
 public class Main {
     
     
-    public static void mains(String[] args) {
-        System.out.println(12);
-
-        System.out.println(12);
-        System.out.println(12);
-        System.out.println(12);
-        System.out.println(12);
-    } 
-    public static void main(String[] args) {
-System.out.println(12);
-
-        System.out.println(12);
-        System.out.println(12);
-        System.out.println(12);
-        System.out.println(12);
-    }
-    
-    
+   
     public static void mainsd(String[] args) {
         System.out.println(12);
 
