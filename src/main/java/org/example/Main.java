1e5f1cf6eaf1a2a17339cf2fc8576d60724a2159
Denis <67637009+Denis-prig@users.noreply.github.com>
2022-09-19 22:48:49 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,7 @@ import javax.swing.event.ListDataEvent;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(10);
-        System.out.println(10);
-        System.out.println(110);
-
-
-        
-         System.out.println(10);
-        System.out.println(10);
-        System.out.println(110);
-
-        float x1 = 12999;
+      
     }
                            
                 
