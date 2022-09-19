20bde0344320bf1967b4cac69724291e1bb10212
Denis <67637009+Denis-prig@users.noreply.github.com>
2022-09-19 22:36:53 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,24 +9,13 @@ public class Main {
         System.out.println(110);
 
 
+        
+         System.out.println(10);
+        System.out.println(10);
+        System.out.println(110);
 
-        float x1 = 12;
+        float x1 = 12999;
     }
                            
-     public static void mains(String[] args) {
-        System.out.println(10;
-        System.out.println(10);
-        System.out.println(110;
-        int x = 10;
-        System.out.println(10);
-        System.out.println(10);
-        System.out.println(110;
-        System.out.println(10);
-        System.out.println(10);
-        System.out.println(110;
-
-
-
-        double x1 = 10;
-    }                       
+                
 }
